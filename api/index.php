773e73ef91e27f83b9c36cc5f9da3175823cf2c3
AntreasCh <99397864+AntreasCh@users.noreply.github.com>
2023-04-02 20:26:18 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -44,6 +44,100 @@ try{
             case '/dashboard':
             $endpoint = new dashboard();
             break;
+       case '/activities':
+                    $endpoint = new Activities();
+                    break;
+                    case '/activitiesimg':
+                        $endpoint = new Activitiesimg();
+                        break;
+                    case '/projects':
+                        $endpoint = new Projects();
+                        break;
+                        case '/projects_text':
+                            $endpoint = new Projects_Text();
+                            break;
+                            case '/demonstrators_text':
+                             $endpoint = new Demonstrators_Text();
+                        break;
+                        case '/demonstrators_projects':
+                         $endpoint = new  Demonstrators_Projects();
+                            break;
+                        case'/updateprojectext':
+                         $endpoint = new UpdateProjectText();
+                         break;
+                         case'/updatedemonstratorstext':
+                              $endpoint = new UpdateText();
+                                 break;
+                            case'/insertprojecttext':
+                             $endpoint = new InsertProjectText();
+                              break;
+                                 case'/demonstratorsprojecttext':
+                                 $endpoint = new InsertDemonstratorsText();
+                                  break;
+                                      case'/deleteprojecttext':
+                                         $endpoint = new DeleteProjectText();
+                                              break;
+                                              case'/insertproject':
+                                                   $endpoint = new InsertProject();
+                                                            break;
+                                                            case'/updateproject':
+                                                                $endpoint = new UpdateProject();
+                                                                break;
+                                                                case'/deleteproject':
+                                                                    $endpoint = new DeleteProject();
+                                                                    break;  
+                                                                    case'/deletedemonstratorstext':
+                                                                        $endpoint = new DeleteDemonstratorsText();
+                                                                        break; 
+                                                                        case'/insertdemonstratorsproject':
+                                                                            $endpoint = new InsertDemonstratorsProject();
+                                                                            break;
+                                                                            case'/deletedemonstratorsproject':
+                                                                                $endpoint = new DeleteDemonstratorsProject();
+                                                                                break;     
+                                                                                case'/updatedemonstratorsproject':
+                                                                                    $endpoint = new UpdateDemonstratorsProject();
+                                                                                    break;
+                                                          
+                                                                                    
+                                                    case'/updateactivitiestext':
+                                        $endpoint = new UpdateActivitiesText();
+                                     break;
+                                     case'/updateactivitiesimg':
+                                        $endpoint = new UpdateActivitiesImg();
+                                     break;
+                                    case'/insertactivitiestext':
+                                   $endpoint = new InsertActivitiesText();
+                                    break;
+                                    case'/deleteactivitiestext':
+                                        $endpoint = new DeleteActivitiesText();
+                                         break;
+
+
+                                         case'/deleteinnovationtext':
+                                            $endpoint = new DeleteInnovationText();
+                                            break; 
+                                            case'/insertinnovationproject':
+                                                $endpoint = new InsertInnovationProject();
+                                                break;
+                                                case'/deleteinnovationproject':
+                                                    $endpoint = new DeleteInnovationProject();
+                                                    break;     
+                                                    case'/updateinnovationproject':
+                                                        $endpoint = new UpdateInnovationProject();
+                                                        break;      
+                                                        case '/innovation_text':
+                                                            $endpoint = new Innovation_Text();
+                                                       break;
+                                                       case '/innovation_projects':
+                                                        $endpoint = new  Innovation_Projects();
+                                                           break;
+                                                           case'/innovationprojecttext':
+                                                            $endpoint = new InsertInnovationText();
+                                                             break;
+                                                             case'/updateinnovationtext':
+                                                                $endpoint = new UpdateInnovationText();
+                                                                   break;
         default:
             $endpoint = new ClientError("Path not found: " . $path, 404);
     }
